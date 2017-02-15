ce3b528a79512748d10b029cc7703fcd8b583ebe
cmyoder <cmyoder@uchicago.edu>
2017-02-15 14:49:33 -0600

    passing all but new movie form thing

--- app/models/director.rb
+++ app/models/director.rb
@@ -1,5 +1,6 @@
 class Director < ApplicationRecord
 
   validates :name, :presence => true, :uniqueness => {:scope => :dob}
+  has_many :movies, :class_name => "Movie", :foreign_key => "director_id"
 
 end
